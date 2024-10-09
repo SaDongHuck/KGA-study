e03f7315b87811ee171bd04b2921b73adf175afa
SaDongHuck <tkehdgur0001@gmail.com>
2024-10-09 16:49:25 +0900

    2024/10/08
    2024/10/08 C# 클래스 과제 입니다

---
+++ 2024-10-08 과제/exam1/exam1/Program.cs
@@ -0,0 +1,106 @@
+﻿//1.
+//- 공격, 방어, 움직임 등등 인터페이스를 만들고
+//- 특정 캐릭터(3개)가 상속을 받는다.
+//ㄴ 각각의 캐릭터들이 행동을 수행하는 결과를 출력한다.
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Principal;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace exam1
+{
+    internal class Program
+    {
+        public interface IAttack
+        {
+            void Attack();
+        }
+
+        public interface IMove
+        {
+            void Move();
+        }
+
+        public interface IDefence
+        {
+            void Defence();
+        }
+
+
+        public class player : IAttack, IMove, IDefence
+        {
+            private string name;
+            private int hp;
+            private int damage;
+            private int defense;
+            public player()
+            {
+                name = "전사";
+                hp = 100;
+                damage = 5;
+                defense = 2;
+            }
+            public void Attack()
+            {
+                Console.WriteLine($"{name}이 공격 합니다");
+                Console.WriteLine($"{damage} 만큼 공격 합니다");
+            }
+            public void Move()
+            {
+                Console.WriteLine($"{name}이 움직입니다");
+            }
+
+            public void Defence()
+            {
+                
+                Console.WriteLine($"{name}이 방어 합니다");
+                Console.WriteLine($"{defense}만큼 방어 합니다");
+            }
+           
+        }
+
+        public class monster : IAttack, IMove, IDefence
+        {
+            private string name;
+            private int hp;
+            private int attack;
+            private int defense;
+            public monster()
+            {
+                name = "시그너스";
+                hp = 100;
+                attack = 8;
+                defense = 8;
+            }
+            public void Attack()
+            {
+                Console.WriteLine($"{name}이 공격 합니다");
+                Console.WriteLine($"{attack} 만큼 공격 합니다");
+            }
+            public void Move()
+            {
+                Console.WriteLine($"{name}이 움직입니다");
+            }
+
+            public void Defence()
+            {
+
+                Console.WriteLine($"{name}이 방어 합니다");
+                Console.WriteLine($"{defense}만큼 방어 합니다");
+            }
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            player p = new player();
+            monster m = new monster();
+            m.Move();
+            p.Move();
+            m.Attack();
+            p.Defence();
+            p.Attack(); 
+            m.Defence();
+        }
+    }
+}
---
+++ 2024-10-08 과제/exam2/Program.cs
@@ -0,0 +1,187 @@
+﻿//2.클래스를 활용하여 대전게임을 만들어라.(프로퍼티 활용)
+using System;
+using System.Threading;
+using System.Diagnostics;
+namespace exam2
+{
+    internal class Program
+    {
+        public abstract class Character
+        {
+            public string Name { get; set; }
+            public int Health { get; set; }
+            public int Attack { get; set; }
+           
+
+            protected Character(string name, int health, int attack, int defence)
+            {
+                Name = name;
+                Health = health;
+                Attack = attack;
+            }
+
+            public virtual void Attack1(Character target)
+            {
+                Console.WriteLine($"{Name}이 공격 합니다");
+                target.Health -= Attack;
+                Console.WriteLine($"{Name}은 {Attack} 만큼 공격 했습니다");
+            }
+
+            public virtual void status()
+            {
+                Console.WriteLine($"플레이 이름 : {Name}");
+                Console.WriteLine($"플레이 채력 : {Health}");
+                Console.WriteLine($"플레이 공격력 : {Attack}");
+            }
+
+
+        }
+
+        public class Thief : Character
+        {
+            public Thief(string name, int health, int attack, int defence)
+                : base(name, health, attack, defence) { }
+
+            public override void Attack1(Character target)
+            {
+                Console.WriteLine($"{Name}이 공격 합니다");
+                target.Health -= Attack;
+                Console.WriteLine($"{Name}은 {Attack} 만큼 공격 했습니다");
+            }
+
+            public void skill(Character target)
+            {
+                string schoice;
+                int choice;
+                Console.WriteLine("스킬을 선택 하시오");
+                Console.WriteLine("1.럭키세븐(30)");
+                Console.WriteLine("2.트리플 스로우(20)");
+                Console.WriteLine("3.슈리켄 첼린지(25)");
+                Console.WriteLine("4.써든 레이드(40)");
+                schoice = Console.ReadLine();
+                choice = Convert.ToInt32(schoice);
+                int skillDamage = 0;
+                switch (choice)
+                {
+                    case 1:
+                        skillDamage = 7;
+                        break;
+                    case 2:
+                        skillDamage = 8;
+                        break;
+                    case 3:
+                        skillDamage = 6;
+                        break;
+                    case 4:
+                        skillDamage = 9;
+                        break;
+                }
+                Console.WriteLine($"{Name}이 스킬을 사용 합니다");
+                target.Health -= skillDamage;
+                Console.WriteLine($"{target.Name}의 채력은 {target.Health}입니다");
+            }
+
+            public override void status()
+            {
+                Console.WriteLine($"플레이 이름 : {Name}");
+                Console.WriteLine($"플레이 채력 : {Health}");
+                Console.WriteLine($"플레이 공격력 : {Attack}");
+            }
+           
+          
+
+        }
+
+        public class Monster : Character
+        {
+            public int f_attack {  get; set; }
+            public Monster(string name, int health, int attack, int defence, int attack2)
+                : base(name, health, attack, defence)
+            {
+                f_attack = attack2;
+            }
+
+            public override void Attack1(Character target)
+            {
+                Console.WriteLine($"{Name}이 공격 합니다");
+                target.Health -= Attack;
+                Console.WriteLine($"{Name}은 {Attack} 만큼 공격 했습니다");
+            }
+
+            public void Attack2(Character target)
+            {
+                Console.WriteLine($"{Name}이 강력한 공격 합니다");
+                target.Health -= f_attack;
+                Console.WriteLine($"{target.Name}의 채력은 {target.Health}입니다");
+            }
+
+            public override void status()
+            {
+                Console.WriteLine($"적 이름 : {Name}");
+                Console.WriteLine($"적 채력 : {Health}");
+                Console.WriteLine($"적 공격력 : {Attack}");
+            }
+
+
+        }
+
+        static void Main(string[] args)
+        {
+            Thief t = new Thief("전사", 100, 5, 8);
+            Monster m = new Monster("시그너스", 100, 6, 5, 9);
+            while(true)
+            {
+                Console.WriteLine("플레이어 순서 입니다");
+                t.status();
+                m.status();
+                Console.WriteLine("1.공격 2.스킬");
+                string schoice;
+                int choice;
+                schoice = Console.ReadLine();
+                choice = Convert.ToInt32(schoice);
+                switch(choice)
+                {
+                    case 1:
+                        t.Attack1(m);
+                        break;
+                    case 2:
+                        t.skill(m);
+                        break;
+                    default:
+                        Console.WriteLine("잘못 입력 하셨습니다");
+                        Console.WriteLine("다시 입력 하세요");
+                        continue;
+                }
+                if(m.Health <= 0)
+                {
+                    Console.WriteLine("이겼습니다");
+                    Console.WriteLine("배틀을 종료 합니다");
+                    break;
+                }
+                Thread.Sleep(3000);
+                Console.WriteLine("몬스터 순서 입니다");
+                Random rand = new Random();
+                int m_input = rand.Next(1, 3);
+                switch(m_input)
+                {
+                    case 1:
+                        m.Attack1(t);
+                        break;
+                    case 2:
+                        m.Attack2(t);
+                        break;
+                    default:
+                        Console.WriteLine("잘못 입력 하셨습니다");
+                        Console.WriteLine("다시 입력 하세요");
+                        continue;
+                }
+                if (t.Health <= 0)
+                {
+                    Console.WriteLine("졌습니다");
+                    Console.WriteLine("배틀을 종료 합니다");
+                    break;
+                }
+            }
+        }
+    }
+}
